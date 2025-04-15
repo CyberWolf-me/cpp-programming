98a7465c0369af80d432f8456961312fa3e72963
CyberWolf-me <akhmedovferuzbek071@gmail.com>
2025-04-15 23:41:22 +0500

    update

--- Passing Parameters/Template_class.cpp
+++ Passing Parameters/Template_class.cpp
@@ -8,26 +8,35 @@ private:
     int b;
 
 public:
-    Arithmetic(T a, T b) : a(a), b(b)
-    {
-    }
-    T add()
-    {
-        return a + b;
-    }
-    T sub()
-    {
-        return a - b
-    }
+    Arithmetic(T a, T b);
+    T add();
+    T sub();
 };
+template <class T>
+Arithmetic<T>::Arithmetic(T a, T b)
+{
+    this->a = a;
+    this->b = b;
+}
+template <class T>
+T Arithmetic<T>::add()
+{
+    return a + b;
+}
+template <class T>
+T Arithmetic<T>::sub()
+{
+    return a - b;
+}
+
 int main()
 {
     Arithmetic<int> r1(10, 5);
-    cout << "Add: " << r1.add();
-    cout << "Subtract: " << r1.sub();
+    cout << "Add: " << r1.add() << endl;
+    cout << "Subtract: " << r1.sub() << endl;
     Arithmetic<float> r2(1.2, 5.2);
-    cout << "Add: " << r2.add();
-    cout << "Subtract: " << r2.sub();
+    cout << "Add: " << r2.add() << endl;
+    cout << "Subtract: " << r2.sub() << endl;
 
     return 0;
 }
