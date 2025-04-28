b39b1f98fb205cc86e5f3926330b5e5c3d3ebbb5
CyberWolf-me <akhmedovferuzbek071@gmail.com>
2025-04-28 22:06:07 +0500

    go

--- Passing Parameters/class_3.cpp
+++ Passing Parameters/class_3.cpp
@@ -39,7 +39,7 @@ public:
     }
     void display()
     {
-        cout << "Area issss:  " << area() << endl;
+        cout << "Area isssss:  " << area() << endl;
         cout << "Perimeter is: " << perimeter() << endl;
     }
 };
